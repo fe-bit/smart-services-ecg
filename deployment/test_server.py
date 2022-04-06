d6a12da252f887588274201d9037c00ac7063138
Patrik Felbinger <patrik.felbinger@web.de>
2022-04-07 01:05:17 +0200

    statistics in testing

--- deployment/test_server.py
+++ deployment/test_server.py
@@ -1,6 +1,7 @@
 import json
 import random
 import requests
+import numpy as np
 url = "https://smart-services-ecg.herokuapp.com/predict"
 
 
@@ -17,6 +18,21 @@ def get_message_for_index(index):
         return "Unclassifiable beat"
 
 
+def get_index_for_message(message):
+    if message == "Normal heartbeat":
+        return 0
+    if message == "Supraventricular premature beat":
+        return 1
+    if message == "Premature ventricular contraction":
+        return 2
+    if message == "Fusion of ventricular and normal beat":
+        return 3
+    if message == "Unclassifiable beat":
+        return 4
+    else:
+        return 5
+
+
 with open(r".\input\mitbih_test.csv") as file:
     all = file.readlines()
 
@@ -27,7 +43,7 @@ def get_rand_line():
     rand = int(random.random() * len(all))
     line = all[rand].replace("\n", "").split(",")
     numbers = [float(number) for number in line]
-    return numbers[: -1], numbers[-1]
+    return numbers[: -1], int(numbers[-1])
 
 
 def make_request():
@@ -35,16 +51,29 @@ def make_request():
     data = {"ecg_data": line}
     r = requests.post(url,
                       json=data)
-    print(r.status_code)
-    print(r.json(), "\tExpected is", get_message_for_index(expected))
+    # print(r.status_code)
+    print("Prediction:", r.json()["prediction"],
+          "\tExpected:", get_message_for_index(expected))
+    return get_index_for_message(r.json()["prediction"]), expected
 
 
-def print_rand_line():
-    l = get_rand_line()
-    for i in l:
-        print(i)
-    print("lenght is", len(l))
+def get_accuracy(matrix, epochs):
+    return np.diag(matrix).sum()/epochs
+
+
+def accuracy_without_indefinite_normal_heartbeat(matrix, epochs):
+    count_normal_indefinite = matrix[5, 0]
+    new_epochs = epochs - count_normal_indefinite
+    return get_accuracy(matrix, new_epochs)
 
 
 if __name__ == "__main__":
-    make_request()
+    matrix = np.zeros((6, 6))
+    epochs = 10
+    for i in range(0, epochs):
+        result, expected = make_request()
+        matrix[result, expected] += 1
+    print(matrix)
+    print("Accuracy: %.2f" % get_accuracy(matrix, epochs))
+    print("Adjusted Accuracy: %.2f" %
+          accuracy_without_indefinite_normal_heartbeat(matrix, epochs))
