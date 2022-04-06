88d95bbc0ee238219eae7467fbaa692c2a847ed0
Patrik Felbinger <patrik.felbinger@web.de>
2022-04-07 01:05:40 +0200

    new tests in dev

--- development/test_server.py
+++ development/test_server.py
@@ -1,7 +1,11 @@
 import requests
 import random
 import json
+import numpy as np
+
+
 url = "http://127.0.0.1:5000/predict"
+
 
 def get_message_for_index(index):
     if index == 0:
@@ -16,6 +20,21 @@ def get_message_for_index(index):
         return "Unclassifiable beat"
 
 
+def get_index_for_message(message):
+    if message == "Normal heartbeat":
+        return 0
+    if message == "Supraventricular premature beat":
+        return 1
+    if message == "Premature ventricular contraction":
+        return 2
+    if message == "Fusion of ventricular and normal beat":
+        return 3
+    if message == "Unclassifiable beat":
+        return 4
+    else:
+        return 5
+
+
 with open(r".\input\mitbih_test.csv") as file:
     all = file.readlines()
 
@@ -26,7 +45,7 @@ def get_rand_line():
     rand = int(random.random() * len(all))
     line = all[rand].replace("\n", "").split(",")
     numbers = [float(number) for number in line]
-    return numbers[: -1], numbers[-1]
+    return numbers[: -1], int(numbers[-1])
 
 
 def make_request():
@@ -34,27 +53,17 @@ def make_request():
     data = {"ecg_data": line}
     r = requests.post(url,
                       json=data)
-    print(r.status_code)
-    print(r.json(), "\tExpected is", get_message_for_index(expected))
-
-
-def print_rand_line():
-    l = get_rand_line()
-    for i in l:
-        print(i)
-    print("lenght is", len(l))
+    # print(r.status_code)
+    print("Prediction:", r.json()["prediction"],
+          "\tExpected:", get_message_for_index(expected))
+    return get_index_for_message(r.json()["prediction"]), expected
 
 
 if __name__ == "__main__":
-    make_request()
-
-
-def print_rand_line():
-    l = get_rand_line()
-    for i in l:
-        print(i)
-    print("lenght is", len(l))
-
-
-if __name__ == "__main__":
-    make_request()
+    matrix = np.zeros((6, 6))
+    epochs = 20
+    for i in range(0, epochs):
+        result, expected = make_request()
+        matrix[result, expected] += 1
+    print(matrix)
+    print("Accuracy:", np.diag(matrix).sum()/epochs)
